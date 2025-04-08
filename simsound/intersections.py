254dd94d44e995f954fd04f7e87087eb0f738f7c
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-04-08 10:40:13 +0200

    intersections refactoring

--- simsound/intersections.py
+++ simsound/intersections.py
@@ -6,40 +6,67 @@ import enum
 import math
 
 
-class Direction(enum.Enum):
-    HORIZONTAL = 0
-    VERTICAL = 1
-
-
 class Intersection:
     __distance: float
-    __direction: Direction
 
-    def __init__(self, distance: float, direction: Direction):
+    def __init__(self, distance: float):
         self.__distance = distance
-        self.__direction = direction
 
     @property
     def distance(self) -> float:
         return self.__distance
 
-    @property
-    def direction(self) -> Direction:
-        return self.__direction
 
-    def approx(self, other: Intersection) -> bool:
-        return math.isclose(self.distance, other.distance, abs_tol=0.001) and self.direction is other.direction
+class VerticalIntersection(Intersection):
+    __x: int
+
+    def __init__(self, distance: float, x: int):
+        super().__init__(distance)
+        self.__x = x
+
+    @property
+    def x(self) -> int:
+        return self.__x
+
+    def __str__(self) -> str:
+        return f"VerticalIntersection(distance={self.distance})"
+
+    def __repr__(self) -> str:
+        return f"VerticalIntersection({self.distance}, {self.x})"
+
+    def approx(self, other: VerticalIntersection) -> bool:
+        return math.isclose(self.distance, other.distance, abs_tol=0.001) and self.x == other.x
 
     def __eq__(self, other: Any) -> bool:
-        if not isinstance(other, Intersection):
+        if not isinstance(other, VerticalIntersection):
             return NotImplemented
         return self.approx(other)
 
+
+class HorizontalIntersection(Intersection):
+    __y: int
+
+    def __init__(self, distance: float, y: int):
+        super().__init__(distance)
+        self.__y = y
+
+    @property
+    def y(self) -> int:
+        return self.__y
+
     def __str__(self) -> str:
-        return f"Intersection(distance={self.distance}, direction={self.direction})"
+        return f"HorizontalIntersection(distance={self.distance})"
 
     def __repr__(self) -> str:
-        return f"Intersection({self.distance}, {self.direction})"
+        return f"HorizontalIntersection({self.distance}, {self.y})"
+
+    def approx(self, other: HorizontalIntersection) -> bool:
+        return math.isclose(self.distance, other.distance, abs_tol=0.001) and self.y == other.y
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, HorizontalIntersection):
+            return NotImplemented
+        return self.approx(other)
 
 
 def find_grid_intersections(ray: Ray) -> Iterable[Intersection]:
@@ -47,6 +74,11 @@ def find_grid_intersections(ray: Ray) -> Iterable[Intersection]:
     dht: float
     vt: float
     dvt: float
+
+    x = math.floor(ray.origin.x) if ray.direction.x < 0 else math.ceil(ray.origin.x)
+    y = math.floor(ray.origin.y) if ray.direction.y < 0 else math.ceil(ray.origin.y)
+    dx = 1 if ray.direction.x > 0 else -1
+    dy = 1 if ray.direction.y > 0 else -1
 
     if ray.direction.x == 0:
         ht = float('inf')
@@ -66,8 +98,10 @@ def find_grid_intersections(ray: Ray) -> Iterable[Intersection]:
 
     while True:
         if ht < vt:
-            yield Intersection(ht, Direction.VERTICAL)
+            yield VerticalIntersection(ht, x)
+            x += dx
             ht += dht
         else:
-            yield Intersection(vt, Direction.HORIZONTAL)
+            yield HorizontalIntersection(vt, y)
+            y += dy
             vt += dvt
--- tests/intersections_tests.py
+++ tests/intersections_tests.py
@@ -1,41 +1,43 @@
-from simsound.intersections import Intersection, find_grid_intersections, Direction
+from simsound.intersections import HorizontalIntersection, VerticalIntersection, find_grid_intersections
 from simsound.ray import Ray, Vector2
 import itertools
 import pytest
 
 
+@pytest.mark.parametrize("x", range(-10, 10))
 @pytest.mark.parametrize("y", range(-10, 10))
-def test_intersections_horizontal_ray(y):
-    ray = Ray(Vector2(0, y), Vector2(1, 0))
+def test_intersections_horizontal_ray(x, y):
+    ray = Ray(Vector2(x, y), Vector2(1, 0))
     intersections = find_grid_intersections(ray)
     for index, intersection in enumerate(itertools.islice(intersections, 10)):
-        assert Intersection(index, Direction.VERTICAL).approx(intersection)
+        assert VerticalIntersection(index, x + index).approx(intersection)
 
 
 @pytest.mark.parametrize("x", range(-10, 10))
-def test_intersections_vertical_ray(x):
-    ray = Ray(Vector2(x, 0), Vector2(0, 1))
+@pytest.mark.parametrize("y", range(-10, 10))
+def test_intersections_vertical_ray(x, y):
+    ray = Ray(Vector2(x, y), Vector2(0, 1))
     intersections = find_grid_intersections(ray)
     for index, intersection in enumerate(itertools.islice(intersections, 10)):
-        assert Intersection(index, Direction.HORIZONTAL).approx(intersection)
+        assert HorizontalIntersection(index, y + index).approx(intersection)
 
 
 def test_intersections():
     ray = Ray(Vector2(0.5, 0.5), Vector2(4, 1))
     intersections = find_grid_intersections(ray)
 
-    assert Intersection(0.125, Direction.VERTICAL) == next(intersections)
-    assert Intersection(0.375, Direction.VERTICAL) == next(intersections)
-    assert Intersection(0.5, Direction.HORIZONTAL) == next(intersections)
-    assert Intersection(0.625, Direction.VERTICAL) == next(intersections)
-    assert Intersection(0.875, Direction.VERTICAL) == next(intersections)
+    assert VerticalIntersection(0.125, 1) == next(intersections)
+    assert VerticalIntersection(0.375, 2) == next(intersections)
+    assert HorizontalIntersection(0.5, 1) == next(intersections)
+    assert VerticalIntersection(0.625, 3) == next(intersections)
+    assert VerticalIntersection(0.875, 4) == next(intersections)
 
 
 def test_intersections_2():
     ray = Ray(Vector2(0.5, 0.5), Vector2(-4, -1))
     intersections = find_grid_intersections(ray)
 
-    assert Intersection(0.125, Direction.VERTICAL) == next(intersections)
-    assert Intersection(0.375, Direction.VERTICAL) == next(intersections)
-    assert Intersection(0.5, Direction.HORIZONTAL) == next(intersections)
-    assert Intersection(0.625, Direction.VERTICAL) == next(intersections)
+    assert VerticalIntersection(0.125, 0) == next(intersections)
+    assert VerticalIntersection(0.375, -1) == next(intersections)
+    assert HorizontalIntersection(0.5, 0) == next(intersections)
+    assert VerticalIntersection(0.625, -2) == next(intersections)
